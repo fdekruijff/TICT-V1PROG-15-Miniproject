a50062e1b994ca092bff361ab329a399f13d09e1
Joepieler <bryan.campagne@student.hu.nl>
2017-09-28 13:38:37 +0200

    google matrix werkend met Lengte graad en breedte graad en een for loop per monteur.

--- google Matrix.py
+++ google Matrix.py
@@ -1,16 +1,30 @@
 import googlemaps
-from datetime import datetime
 
-def distance(origin,destination):
-    gmaps = googlemaps.Client(key='AIzaSyB3sE6Ekts-GoPlZ8vJ8P8i0UL1rVFnnPI')
 
-    my_dis = gmaps.distance_matrix(origin,destination)
-    rows = my_dis['rows']
-    row = rows[0]
-    elements = row['elements']
-    element = elements[0]
-    distances = element['distance']
-    distance = distances['text']
-    return (distance)
+def distance(origin, destination):
+    google_maps = googlemaps.Client(key='AIzaSyB3sE6Ekts-GoPlZ8vJ8P8i0UL1rVFnnPI')
+    delta = google_maps.distance_matrix(origin, destination)['rows'][0]['elements'][0]['distance']['text']
+    return delta
 
-print (distance('amersfoort', 'utrecht'))
+kaartautomaat=52.3702157,4.895167899999933
+
+kleinsteafstand = 1111111111111111111111111111111111111111111111111111111111111111
+monteurs= {'tim':[52.1561113,5.387826600000039], 'jan':[52.09073739999999,5.121420100000023], 'dick':[51.9244201,4.4777325999999675]}
+
+
+for Name, GEO in monteurs.items():
+        geo= (tuple(GEO))
+        afstand= (distance(geo, kaartautomaat))
+        afstand= afstand[:-1]
+        afstand= afstand[:-1]
+        afstand= float(afstand)
+
+        if afstand <= kleinsteafstand:
+            kleinsteafstand = afstand
+            kleinsteafstandmonteur = Name
+
+print (kleinsteafstandmonteur)
+
+
+
+
