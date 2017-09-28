20a6d59909099f79b2e78b3fc71ff4d57dcdf71a
Floris de Kruijff <fdekruijff@users.noreply.github.com>
2017-09-28 12:10:54 +0200

    Code update

--- google Matrix.py
+++ google Matrix.py
@@ -1,16 +1,11 @@
 import googlemaps
-from datetime import datetime
 
-def distance(origin,destination):
-    gmaps = googlemaps.Client(key='AIzaSyB3sE6Ekts-GoPlZ8vJ8P8i0UL1rVFnnPI')
 
-    my_dis = gmaps.distance_matrix(origin,destination)
-    rows = my_dis['rows']
-    row = rows[0]
-    elements = row['elements']
-    element = elements[0]
-    distances = element['distance']
-    distance = distances['text']
-    return (distance)
+def distance(origin, destination):
+    google_maps = googlemaps.Client(key='AIzaSyB3sE6Ekts-GoPlZ8vJ8P8i0UL1rVFnnPI')
+
+    delta = google_maps.distance_matrix(origin, destination)['rows'][0]['elements'][0]['distance']['text']
+    return delta
+
 
 print (distance('amersfoort', 'utrecht'))
